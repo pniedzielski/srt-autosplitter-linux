7c1309c74a501536bf4abca7c4fe5f8c9556a9e4
Patrick M. Niedzielski <patrick@pniedzielski.net>
2022-08-15 05:53:18 +0000

    Use WASM `import-module` attribute

--- env.hpp
+++ env.hpp
@@ -1,6 +1,8 @@
 #ifndef LIVESPLIT_ONE_AUTOSPLITTER_ENV_HPP
 #define LIVESPLIT_ONE_AUTOSPLITTER_ENV_HPP
 
+#define ENV __attribute__((import_module("env")))
+
 #include <cstddef>        // std::byte
 #include <cstdint>        // std::uint{32,64}_t, std::int{32,64}_t,
                           // std::size_t
@@ -24,59 +26,61 @@ const auto ENDED       = TimerState{ 3 };
 
 extern "C" {
   // Gets the state that the timer currently is in.
-  auto timer_get_state() -> TimerState;
+  auto ENV timer_get_state() -> TimerState;
 
   // Starts the timer.
-  void timer_start();
+  void ENV timer_start();
   // Splits the current segment.
-  void timer_split();
+  void ENV timer_split();
   // Resets the timer.
-  void timer_reset();
+  void ENV timer_reset();
   // Sets a custom key value pair.  This may be arbitrary information
   // that the auto splitter wants to provide for visualization.
-  void timer_set_variable(const std::byte* key_ptr,
-                          std::size_t      key_len,
-                          const std::byte* value_ptr,
-                          std::size_t      value_len);
+  void ENV timer_set_variable(const std::byte* key_ptr,
+                              std::size_t      key_len,
+                              const std::byte* value_ptr,
+                              std::size_t      value_len);
 
   // Sets the game time.
-  void timer_set_game_time(std::int64_t secs, std::int32_t nanos);
+  void ENV timer_set_game_time(std::int64_t secs, std::int32_t nanos);
   // Pauses the game time.  This does not pause the timer, only the
   // automatic flow of time for the game time.
-  void timer_pause_game_time();
+  void ENV timer_pause_game_time();
   // Resumes the game time.  This does not resume the timer, only the
   // automatic flow of time for the game time.
-  void timer_resume_game_time();
+  void ENV timer_resume_game_time();
 
   // Attaches to a process based on its name.  (Returns 0 on failure.)
-  auto process_attach(const std::byte* name_ptr,
-                      std::size_t      name_len)
+  auto ENV process_attach(const std::byte* name_ptr,
+                          std::size_t      name_len)
     -> ProcessId;
   // Detaches from a process.
-  void process_detach(ProcessId process);
+  void ENV process_detach(ProcessId process);
   // Checks whether is a process is still open.  You should detach
   // from a process and stop using it if this returns `false`.
-  auto process_is_open(ProcessId process) -> bool;
+  auto ENV process_is_open(ProcessId process) -> bool;
   // Reads memory from a process at the address given.  This will
   // write the memory to the buffer given.  Returns `false` if this
   // fails.
-  auto process_read(ProcessId   process,
-                    Address     address,
-                    std::byte*  buf_ptr,
-                    std::size_t buf_len)
+  auto ENV process_read(ProcessId   process,
+                        Address     address,
+                        std::byte*  buf_ptr,
+                        std::size_t buf_len)
     -> bool;
   // Gets the address of a module in a process.  (Returns 0 on failure.)
-  auto process_get_module_address(ProcessId        process,
-                                  const std::byte* name_ptr,
-                                  std::size_t      name_len)
+  auto ENV process_get_module_address(ProcessId        process,
+                                      const std::byte* name_ptr,
+                                      std::size_t      name_len)
     -> NonZeroAddress;
 
   // Sets the tick rate of the runtime.  This influences the amount of
   // times the `update` function is called per second.
-  void runtime_set_tick_rate(double ticks_per_second);
+  void ENV runtime_set_tick_rate(double ticks_per_second);
   // Prints a log message for debugging purposes.
-  void runtime_print_message(const std::byte* text_ptr,
-                             std::size_t      text_len);
+  void ENV runtime_print_message(const std::byte* text_ptr,
+                                 std::size_t      text_len);
 }
+
+#undef ENV
 
 #endif  // #ifndef LIVESPLIT_ONE_AUTOSPLITTER_ENV_HPP
